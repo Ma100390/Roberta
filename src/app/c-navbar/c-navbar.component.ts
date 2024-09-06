8a42a2d36a0e97faac1be0a7668b0ae0c1982011
Manuel <magini15@gmail.com>
2024-09-06 12:52:40 +0200

    first commit

---
+++ src/app/c-navbar/c-navbar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-c-navbar',
+  standalone: true,
+  imports: [],
+  templateUrl: './c-navbar.component.html',
+  styleUrl: './c-navbar.component.css'
+})
+export class CNavbarComponent {
+
+}
