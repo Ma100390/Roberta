5788f21c27ffe033ae2d4610a95071fd48380916
Manuel <magini15@gmail.com>
2024-10-29 14:02:33 +0100

    make it better

--- src/app/p-aree/p-aree.component.ts
+++ src/app/p-aree/p-aree.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-p-aree',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './p-aree.component.html',
   styleUrl: './p-aree.component.css'
 })
