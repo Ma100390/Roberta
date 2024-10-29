3d02eaefd49b5b4541ed09576fdde384d5c6806d
Manuel <magini15@gmail.com>
2024-10-29 15:21:28 +0100

    primo deploy

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// Servire i file statici dall'app Angular buildata
+app.use(express.static(path.join(__dirname, 'dist/app2')));
+
+// Gestire tutte le richieste in modo che vengano servite dall'app Angular
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'dist/app2/index.html'));
+});
+
+// Porta per Heroku
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+  console.log(`Server in esecuzione sulla porta ${port}`);
+});
